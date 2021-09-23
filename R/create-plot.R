22d6c2ccd05924b02cb9737b319968a503135c69
Kasia Noone <kasianoone@gmail.com>
2021-09-23 10:06:20 +0100

    Changed country and added title

--- R/create-plot.R
+++ R/create-plot.R
@@ -9,14 +9,14 @@ df = read_csv("data/weatherAUS.csv")
 # Create a scatter plot of Humidity9am and Temp9am
 # a location
 
-city = "Perth"
-
-scatter_plot = 
-df %>%
-  filter(Location == city) %>%
-  ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour = "red")
-
+scatter_plot =
+  df %>%
+  filter(Location == "Brisbane") %>%
+  ggplot(aes(x = Temp9am, y = Humidity9am)) +
+  geom_point(colour = "red") +
+  labs(title = "Humidity and Temperature for Brisbane") +
+  theme_minimal()
+  labs(caption = "Copyright by the Australian Commonwealth Bureau of Meteorology")
 
 
 # Save the plot in graphics/
